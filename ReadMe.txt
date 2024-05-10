1dfdba3eceaf9de956634bdd3ae497c383dfa2fa
Janidu Chamika <100714228+JaniduChamika@users.noreply.github.com>
2024-05-10 23:06:33 +0530

    Add files via upload

---
+++ ReadMe.txt
@@ -0,0 +1,15 @@
+Test details
+
+Credit Card Number:
+4137354252670921
+
+Expiration Date:
+07/2027
+
+Paypal account 
+
+Email ID:
+sb-vrb6r17121104@personal.example.com
+
+Password:
+B!MJj3Cv
---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+
+<html lang="en" class="light-style customizer-hide" dir="ltr" data-theme="theme-default" data-assets-path="../assets/" data-template="vertical-menu-template-free">
+
+<head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+      <title>Log In</title>
+
+      <meta name="description" content="" />
+
+      <!-- Favicon -->
+      <link rel="icon" type="image/x-icon" href="assets/img/favicon/favicon.ico" />
+
+
+
+      <!-- Icons. Uncomment required icon fonts -->
+      <link rel="stylesheet" href="assets/vendor/fonts/boxicons.css" />
+
+      <!-- Core CSS -->
+      <link rel="stylesheet" href="assets/vendor/css/core.css" class="template-customizer-core-css" />
+      <link rel="stylesheet" href="assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+
+
+      <link rel="stylesheet" href="assets/vendor/css/pages/page-auth.css" />
+      <link rel="stylesheet" href="css/bac-animation.css" />
+</head>
+
+<body>
+      <!-- Content -->
+      <div class="area position-fixed">
+            <ul class="circles">
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+            </ul>
+      </div>
+      <div class="container-xxl">
+            <div class="authentication-wrapper authentication-basic container-p-y">
+                  <div class="authentication-inner">
+                        <!-- Register -->
+                        <div class="card">
+                              <div class="card-body" id="loginbox">
+                                    <!-- Logo -->
+                                    <div class="app-brand justify-content-center">
+                                          <a href="index.html" class="app-brand-link gap-2">
+                                                <span class="app-brand-logo demo">
+                                                      <img src="image/logo/logo.png" width="38" />
+                                                      <!-- logo  -->
+                                                </span>
+                                                <span class="app-brand-text demo text-body fw-bolder fs-3">E-School Log In</span>
+                                          </a>
+                                    </div>
+                                    <!-- /Logo -->
+                                    <!-- login option  -->
+                                    <div class="mb-2 row g-2">
+                                          <a href="login/gui/student-login.php" type="button" class="btn btn-xl btn-info">
+                                                <i class="menu-icon tf-icons bx bxs-graduation fs-xlarge"></i>
+                                                <br />
+                                                Student
+                                          </a>
+                                          <a href="login/gui/teacher-login.php" type="button" class="btn btn-xl btn-info">
+                                                <i class="menu-icon tf-icons bx bx-book-reader fs-xlarge"></i>
+                                                <br />
+                                                Teacher
+                                          </a>
+                                          <a href="login/gui/officer-login.php" type="button" class="btn btn-xl btn-info">
+                                                <i class="menu-icon tf-icons bx bx-glasses-alt fs-xlarge"></i>
+                                                <br />
+                                                Academic Officer
+                                          </a>
+                                          <a href="login/gui/admin-login.php" type="button" class="btn btn-xl btn-info">
+                                                <i class="menu-icon tf-icons bx bxs-binoculars fs-xlarge"></i>
+                                                <br />
+                                                Admin
+                                          </a>
+                                    </div>
+                                    <!-- login option  -->
+
+
+
+                              </div>
+
+
+                        </div>
+                  </div>
+                  <!-- /Register -->
+            </div>
+      </div>
+
+
+      <!-- / Content -->
+      <script src="js/script.js"></script>
+
+
+</body>
+
+</html>
